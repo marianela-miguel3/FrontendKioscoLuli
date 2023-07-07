3c52662fa3654f1aef92fa5527401c67506a5d64
marianela-miguel3 <82850065+marianela-miguel3@users.noreply.github.com>
2023-07-07 17:15:09 -0300

    subiendo frontend

---
+++ frontend/js/main.js
@@ -0,0 +1,19 @@
+document.getElementById("header").innerHTML=`<nav class="navbar navbar-expand-sm navbar-light bg-light">
+<div class="container">
+ 
+  <div class="collapse navbar-collapse" id="collapsibleNavId">
+      <ul class="navbar-nav me-auto mt-2 mt-lg-0">
+          
+          <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="dropdownId" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">KIOSCO</a>
+              <div class="dropdown-menu" aria-labelledby="dropdownId">
+                  <a class="dropdown-item" href="productos.html">Productos</a>
+                  <a class="dropdown-item" href="#">Reservas</a>
+                  <a class="dropdown-item" href="#">Ofertas</a>
+              </div>
+          </li>
+      </ul>
+      
+  </div>
+</div>
+</nav>`
---
+++ frontend/js/productos.js
@@ -0,0 +1,48 @@
+const { createApp } = Vue
+
+  createApp({
+    data() {
+      return {
+        url:"https://kioscoluli.pythonanywhere.com/productos",
+        productos:[],
+        error:false,
+        cargando:true
+      }
+    },
+    // Se llama después de que la instancia haya 
+    // terminado de procesar todas las opciones relacionadas con el estado.
+    created() {
+        this.fetchData(this.url)
+    },
+    methods: {
+        fetchData(url) {
+            fetch(url)
+                .then(response => response.json())
+                .then(data => {
+                    this.productos = data;
+                    this.cargando=false
+                })
+                .catch(err => {
+                    console.error(err);
+                    this.error=true              
+                })
+        },
+        eliminar(producto) {
+            const url = 'https://kioscoluli.pythonanywhere.com/productos/' + producto;
+            var options = {
+                method: 'DELETE',
+            }
+            fetch(url, options)
+                .then(res => res.text()) // or res.json()
+                .then(res => {
+                    location.reload();
+                })
+        }
+
+
+    },
+    
+
+
+
+  }).mount('#app')
